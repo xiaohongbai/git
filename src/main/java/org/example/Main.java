6ca2f63c07d7cb92d2c09adc5f22e30615db2cb3
xiaohongbai <2277176183@qq.com>
2024-09-21 21:03:35 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("master");
-        System.out.println("hot-fix");
+        System.out.println("hot-fix pull");
     }
 }
