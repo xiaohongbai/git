937aab39d5d4cefc6eae49e9ad7b13aedfc4fc25
xinyou <xinyou123@qq.com>
2024-09-21 20:06:01 +0800

    san commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,14 +7,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n= sc.nextInt();
-        ArrayList<Long> arrayList=new ArrayList<>();
-        ArrayList<Long> array=new ArrayList<>();
-        for(int i=0;i<n;i++)
-        {
-            Long a=sc.nextLong();
-            arrayList.add(a);
-        }
+        System.out.println("讨厌刷题");
     }
 }
